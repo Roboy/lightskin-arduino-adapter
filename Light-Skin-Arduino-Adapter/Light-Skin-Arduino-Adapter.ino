e0150c7fa2e8b28e35d2bfe0430300d66199a8bf
Daniel Schubert <daniel.schubert@tum.de>
2018-03-22 02:51:47 +0100

    Some beautifications

--- Light-Skin-Arduino-Adapter/Light-Skin-Arduino-Adapter.ino
+++ Light-Skin-Arduino-Adapter/Light-Skin-Arduino-Adapter.ino
@@ -1,24 +1,23 @@
-
-
-const int LEDs[] = {
-  1,
-  2,
-  3
-};
-const int LEDs_num = sizeof(LEDs) / sizeof(int);
-
 #define LED_OFF HIGH
 #define LED_ON LOW
 
-const int Sensors[] = {
-  A0,
-  A1,
-  A2
+const int LEDs[] = {
+  1, 2, 3
 };
+const int Sensors[] = {
+  A0, A1, A2
+};
+
+
+// Samples to be measured for each sensor measurement
+const int SAMPLES = 50;
+
+
+
+
+const int LEDs_num = sizeof(LEDs) / sizeof(int);
 const int Sensors_num = sizeof(Sensors) / sizeof(int);
 
-
-const int SAMPLES = 50;
 
 
 
@@ -39,13 +38,16 @@ void setup() {
 
 
 float allValues[LEDs_num][Sensors_num];
-
 int lastLED = LEDs[0];
+long sum;
+float value;
+int l, s, i; // Inner loop variables; so they don't need to be reallocated every time
+
 void loop() {
 
   // Take a "snapshot": measure all the LED / Sensor combinations
   
-  for(int l = 0; l < LEDs_num; l++){
+  for(l = 0; l < LEDs_num; l++){
     int led = LEDs[l];
 
     // switch to new LED
@@ -53,16 +55,16 @@ void loop() {
     digitalWrite(led, LED_ON);
     
     // Measure all sensors
-    for(int s = 0; s < Sensors_num; s++){
+    for(s = 0; s < Sensors_num; s++){
       int sensor = Sensors[s];
       // Read sensor once to switch ADC circuit to this pin (discard result)
       analogRead(sensor);
 
-      long sum = 0;
-      for(int i = 0; i < SAMPLES; i++){
+      sum = 0;
+      for(i = 0; i < SAMPLES; i++){
         sum += analogRead(sensor);
       }
-      float value = sum / SAMPLES;
+      value = sum / SAMPLES;
 
       allValues[l][s] = value;
     }
@@ -76,8 +78,8 @@ void loop() {
   Serial.print(',');
   Serial.print(Sensors_num);
   Serial.println();
-  for(int l = 0; l < LEDs_num; l++){
-    for(int s = 0; s < Sensors_num; s++){
+  for(l = 0; l < LEDs_num; l++){
+    for(s = 0; s < Sensors_num; s++){
       Serial.print(allValues[l][s]);
       Serial.print(',');
     }
