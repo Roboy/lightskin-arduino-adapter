139c9de2b8d7f5764c6a59cc85b0baa5957ba093
Daniel Schubert <daniel.schubert@tum.de>
2018-03-24 20:14:39 +0100

    Massively speed up analog sampling

--- Light-Skin-Arduino-Adapter/Light-Skin-Arduino-Adapter.ino
+++ Light-Skin-Arduino-Adapter/Light-Skin-Arduino-Adapter.ino
@@ -1,5 +1,10 @@
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+
+
 #define LED_OFF HIGH
 #define LED_ON LOW
+
 
 const int LEDs[] = {
   49, 47, 45, 43, 41, 39, 37, 35 ,33, 31, 29, 27
@@ -10,7 +15,7 @@ const int Sensors[] = {
 
 
 // Samples to be measured for each sensor measurement
-const int SAMPLES = 2;
+const int SAMPLES = 5;
 
 
 
@@ -22,6 +27,11 @@ const int Sensors_num = sizeof(Sensors) / sizeof(int);
 
 
 void setup() {
+  // Increase ADC sampling rate according to 
+  // https://yaab-arduino.blogspot.de/2015/02/fast-sampling-from-analog-input.html
+  sbi(ADCSRA, ADPS2);
+  cbi(ADCSRA, ADPS1);
+  cbi(ADCSRA, ADPS0);
 
   for(int i = 0; i < LEDs_num; i++){
     pinMode(LEDs[i], OUTPUT);
@@ -31,7 +41,7 @@ void setup() {
     pinMode(Sensors[i], INPUT);
   }
   
-  Serial.begin(115200);
+  Serial.begin(2000000);
 }
 
 
@@ -44,10 +54,12 @@ float value;
 int l, s, i; // Inner loop variables; so they don't need to be reallocated every time
 
 void loop() {
+  long t0, t;
+  t0 = micros();
 
   // Take a "snapshot": measure all the LED / Sensor combinations
   
-  for(l = 0; l < LEDs_num; l++){
+  /*for(l = 0; l < LEDs_num; l++){
     int led = LEDs[l];
 
     // switch to new LED
@@ -72,7 +84,41 @@ void loop() {
     lastLED = led;
   }
   digitalWrite(lastLED, LED_OFF);
+  /* */
 
+  
+  int led;
+  // Measure all sensors
+  for(s = 0; s < Sensors_num; s++){
+    int sensor = Sensors[s];
+    // Read sensor once to switch ADC circuit to this pin (discard result)
+    analogRead(sensor);
+
+    for(l = 0; l < LEDs_num; l++){
+      led = LEDs[l];
+  
+      // switch to new LED
+      digitalWrite(lastLED, LED_OFF);
+      digitalWrite(led, LED_ON);
+  
+      sum = 0;
+      for(i = 0; i < SAMPLES; i++){
+        sum += analogRead(sensor);
+      }
+      value = (float) sum / SAMPLES;
+  
+      allValues[l][s] = value;
+      
+      lastLED = led;
+    }
+    digitalWrite(lastLED, LED_OFF);
+  }
+  /* */
+
+  
+  t = micros();
+
+  
   // Print the measurements to Serial
   //Serial.print("Snapshot: ");
   //Serial.print(LEDs_num);
@@ -88,6 +134,9 @@ void loop() {
     //Serial.println();
   }
   Serial.println();
+  /* */
+
+  //Serial.println(t-t0);
 
   // Wait for the next snapshot to be taken
   //delay(100);
